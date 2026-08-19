521675a59dc5f7615d8f4340998020988ae2e8ba
Kuldeep verma <kuldeepup112@gmail.com>
2026-08-19 13:14:06 +0530

    Restore calculator functionality and user prompts

--- Calculater2.py
+++ Calculater2.py
@@ -1,56 +1,40 @@
 
-# print("Project do work of the calculater 🧮🧮🧮")
-# print('chose the number : \n1. addtion\n2. subtraction\n3. multiplecation \n4. division')
+print("Project do work of the calculater 🧮🧮🧮")
+print('chose the number : \n1. addtion\n2. subtraction\n3. multiplecation \n4. division')
 
-# def add(num1,num2):
-#     return num1+num2
+def add(num1,num2):
+    return num1+num2
 
-# def sub(num1,num2):
-#     return num1-num2
-
-
-# def mul(num1,num2):
-#     return num1*num2
+def sub(num1,num2):
+    return num1-num2
 
 
-# def div(num1,num2):
-#     return num1/num2
-
-# Select = int(input ('Select the operater  1 - 4 : '))
-
-# number1 = int(input("Enter the first number : "))
-# number2 = int(input("Enter the second number : "))
-
-# if Select ==1:
-#     print(add(number1,number2))
-
-# elif Select ==2:
-#     print(sub(number1,number2))
+def mul(num1,num2):
+    return num1*num2
 
 
-# elif Select ==3:
-#     print(mul(number1,number2))
+def div(num1,num2):
+    return num1/num2
+
+Select = int(input ('Select the operater  1 - 4 : '))
+
+number1 = int(input("Enter the first number : "))
+number2 = int(input("Enter the second number : "))
+
+if Select ==1:
+    print(add(number1,number2))
+
+elif Select ==2:
+    print(sub(number1,number2))
+
+
+elif Select ==3:
+    print(mul(number1,number2))
     
 
-# elif Select ==4:
-#     print(div(number1,number2))
+elif Select ==4:
+    print(div(number1,number2))
 
 
 
-
-def calculate(a, b):
-    return a + b
-
-
-
-while True:
-    a = int(input("First Number: "))
-    b = int(input("Second Number: "))
-
-    op = input("Operator: ")
-
-    if op == '+':
-        print(a+b)
-
-    name = calculate(a,b)
   
